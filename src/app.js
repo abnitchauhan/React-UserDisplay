91e6d8b2134face7d394218f371fd386995e10e7
Abnit Chauhan <abnitchauhanabnit@gmail.com>
2021-02-12 17:40:41 +0530

    React app with Used Data Displayed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import store from '../store/store';
 import {addUsers} from './actions/users';
 import Header from './components/Header';
 import { Provider } from 'react-redux';
+import UsersList from './components/UsersList';
+
 
 class App extends React.Component {
    
@@ -13,18 +15,21 @@ class App extends React.Component {
         axios.get('http://localhost:3000/users')
         .then(response=>{
             console.log(response.data);
+            
             store.dispatch(addUsers(response.data.results))
-        })
+        });
     }
 
     render()
     {
+        
         return(<Header />);
     }
 }
 
 ReactDOM.render(
-<Provider store= {store} >
-<App />
-</Provider>
-, document.getElementById('root'));
+    <Provider store={store}>
+     <App />
+     <UsersList />
+    </Provider>,
+   document.getElementById('root'));
